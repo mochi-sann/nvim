f6e5680304cbfc747f3799dc30ae3fdea88a76d4
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-05-06 19:05:08 +0900

    よくわからん

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
-"vim.opt.number=true;
+vim.opt.number=true;
 vim.opt.updatetime=300;
 vim.opt.laststatus=3;
+local globalSettings ={
+    number = true,
+    updatetime = 300,
+    showtabline = 2 ,
+
+}
+vim.opt = globalSettings;
