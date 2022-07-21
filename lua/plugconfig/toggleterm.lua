e0c5a6708355a519b2d8291fe829729b167eed88
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-07-21 21:15:12 +0900

    toglletermの設定を追加した

--- lua/plugconfig/toggleterm.lua
+++ lua/plugconfig/toggleterm.lua
@@ -3,3 +3,12 @@ require("toggleterm").setup({
 	-- size = 20,
 	direction = "tab",
 })
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
