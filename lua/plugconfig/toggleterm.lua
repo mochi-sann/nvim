96f6f3bed00bb3179207e53d9a025b4bb1973c9e
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-07-21 21:23:04 +0900

    update lazygit keybind

--- lua/plugconfig/toggleterm.lua
+++ lua/plugconfig/toggleterm.lua
@@ -11,4 +11,4 @@ function _lazygit_toggle()
 	lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
