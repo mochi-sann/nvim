e392812751f6d369221e29528740237f0f4db019
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-07-30 16:39:28 +0900

    update

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,358 +1,367 @@
 vim.cmd([[packadd packer.nvim]])
 return require("packer").startup(function()
-	-- Packer can manage itself
-	use({ "wbthomason/packer.nvim" })
+  -- Packer can manage itself
+  use({ "wbthomason/packer.nvim" })
 
-	use({ "junegunn/fzf", run = ":call fzf#install()" })
-	-- fizzy finder
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = "nvim-lua/plenary.nvim",
-		config = function()
-			require("plugconfig/telescope")
-		end,
-	})
+  use({ "junegunn/fzf", run = ":call fzf#install()" })
+  use({ "nvim-lua/popup.nvim" })
 
-	-- LSP settings
-	use({
-		"hrsh7th/nvim-cmp",
-		config = function()
-			require("plugconfig/nvim_cmp")
-		end,
-	})
+  -- fizzy finder
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("plugconfig/telescope")
+    end,
+  })
 
-	use({ "hrsh7th/cmp-nvim-lsp" })
-	use({ "hrsh7th/cmp-buffer" })
-	use({ "hrsh7th/cmp-path" })
-	use({ "hrsh7th/cmp-cmdline" })
-	use({ "hrsh7th/cmp-nvim-lsp-signature-help" })
-	use({ "hrsh7th/cmp-nvim-lua" })
-	use({ "hrsh7th/cmp-emoji" })
-	use({ "hrsh7th/cmp-nvim-lsp-document-symbol" })
-	use({ "hrsh7th/cmp-vsnip" })
-	-- use({
-	-- 	"zbirenbaum/copilot-cmp",
-	-- 	after = { "copilot.lua", "nvim-cmp" },
-	-- })
-	-- use({
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	config = function()
-	-- 		vim.defer_fn(function()
-	-- 			require("copilot").setup()
-	-- 		end, 100)
-	-- 	end,
-	-- })
-	-- use({ "github/copilot.vim" })
-	-- use({ "vim-skk/skkeleton", requires = { "vim-denops/denops.vim" } })
-	-- use({ "rinx/cmp-skkeleton", after = { "nvim-cmp", "skkeleton" } })
-
-	use({
-		"tzachar/cmp-tabnine",
-		run = "./install.sh",
-		requires = "hrsh7th/nvim-cmp",
-		config = function()
-			require("plugconfig/cmp-tabnine")
-		end,
-	})
-
-	-- use({
-	-- 	"TimUntersberger/neogit",
-	-- 	requires = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"sindrets/diffview.nvim",
-	-- 	},
-	-- 	config = function()
-	-- 		require("plugconfig/neogit")
-	-- 	end,
-	-- 	-- opt = true,
-	-- 	-- event = "VimEnter",
-	-- })
-	use({
-		"pwntester/octo.nvim",
-		event = "VimEnter",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"kyazdani42/nvim-web-devicons",
-		},
-		config = function()
-			require("octo").setup()
-		end,
-	})
-	use({
-		"phaazon/hop.nvim",
-		branch = "v2", -- optional but strongly recommended
-		event = "VimEnter",
-		config = function()
-			require("plugconfig/hop")
-		end,
-	})
-
-	--
-	use({
-		"onsails/lspkind.nvim",
-		config = function()
-			require("plugconfig/lspkind")
-		end,
-	})
-	-- use({
-	-- 	"williamboman/nvim-lsp-installer",
-	-- }),
-	use({ "neovim/nvim-lspconfig" })
-  use( { "williamboman/mason.nvim" })
-  use( { "williamboman/mason-lspconfig.nvim" })
+  -- LSP settings
+  use({
+    "hrsh7th/nvim-cmp",
+    config = function()
+      require("plugconfig/nvim_cmp")
+    end,
+  })
 
 
-	-- use({
-	-- 	"hrsh7th/cmp-vsnip",
-	-- 	"hrsh7th/vim-vsnip",
-	-- 	after = "nvim-cpm",
-	-- 	config = function()
-	-- 		require("plugconfig/vsnip")
-	-- 	end,
-	-- })
-	use({
-		"hrsh7th/vim-vsnip",
-		config = function()
-			require("plugconfig/vsnip")
-		end,
-		requires = {
-			{ "hrsh7th/vim-vsnip-integ" },
-		},
-	})
-	--use({ "hrsh7th/cmp-vsnip", after = "nvim-cpm" })
-	-- format and linter
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		config = function()
-			require("plugconfig/null_ls")
-		end,
-	})
-	--use({"lambdalisue/fern.vim" , opt = true, cmd = {'Fern'}})
+  use({ "hrsh7th/cmp-nvim-lsp" })
+  use({ "hrsh7th/cmp-buffer" })
+                  use({ "hrsh7th/cmp-path" })
+  use({ "hrsh7th/cmp-cmdline" })
+  use({ "hrsh7th/cmp-nvim-lsp-signature-help" })
+  use({ "hrsh7th/cmp-nvim-lua" })
+  use({ "hrsh7th/cmp-emoji" })
+  use({ "hrsh7th/cmp-nvim-lsp-document-symbol" })
+  use({ "hrsh7th/cmp-vsnip" })
+  -- use({
+  -- 	"zbirenbaum/copilot-cmp",
+  -- 	after = { "copilot.lua", "nvim-cmp" },
+  -- })
+  -- use({
+  -- 	"zbirenbaum/copilot.lua",
+  -- 	config = function()
+  -- 		vim.defer_fn(function()
+  -- 			require("copilot").setup()
+  -- 		end, 100)
+  -- 	end,
+  -- })
+  -- use({ "github/copilot.vim" })
+  -- use({ "vim-skk/skkeleton", requires = { "vim-denops/denops.vim" } })
+  -- use({ "rinx/cmp-skkeleton", after = { "nvim-cmp", "skkeleton" } })
 
-	use({
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		event = "VimEnter",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("plugconfig/neo-tree")
-		end,
-	})
-	-- use({
-	-- 	"lambdalisue/fern.vim",
-	-- 	opt = true,
-	-- 	event = "VimEnter",
-	--
-	-- 	config = function()
-	-- 		require("plugconfig/fern")
-	-- 	end,
-	-- })
-	-- use({ "lambdalisue/fern-git-status.vim", after = "fern.vim" })
-	-- -- use({ "lambdalisue/nerdfont.vim", after = "fern.vim" })
-	-- use({ "lambdalisue/fern-renderer-nerdfont.vim", after = "fern.vim", requires = { "lambdalisue/nerdfont.vim" } })
-	-- use({ "lambdalisue/glyph-palette.vim", after = "fern.vim" })
-	-- use({ "lambdalisue/fern-bookmark.vim", after = "fern.vim" })
-	use({
-		"t9md/vim-quickhl",
-		event = "VimEnter",
-		config = function()
-			require("plugconfig/vim_quichl")
-		end,
-	})
-	use({
-		"terryma/vim-expand-region",
-		event = "VimEnter",
-		config = function()
-			require("plugconfig/vim_expand_region")
-		end,
-	})
-	use({ "segeljakt/vim-silicon", event = "VimEnter" })
-	-- colorschem
-	use({ "dracula/vim", as = "dracula" })
-	-- use({ "sainnhe/sonokai" })
+  use({
+    "tzachar/cmp-tabnine",
+    run = "./install.sh",
+    requires = "hrsh7th/nvim-cmp",
+    config = function()
+      require("plugconfig/cmp-tabnine")
+    end,
+  })
 
-	--use("rebelot/kanagawa.nvim")
+  -- use({
+  -- 	"TimUntersberger/neogit",
+  -- 	requires = {
+  -- 		"nvim-lua/plenary.nvim",
+  -- 		"sindrets/diffview.nvim",
+  -- 	},
+  -- 	config = function()
+  -- 		require("plugconfig/neogit")
+  -- 	end,
+  -- 	-- opt = true,
+  -- 	-- event = "VimEnter",
+  -- })
+  use({
+    "pwntester/octo.nvim",
+    event = "VimEnter",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "kyazdani42/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup()
+    end,
+  })
+  use({
+    "phaazon/hop.nvim",
+    branch = "v2", -- optional but strongly recommended
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/hop")
+    end,
+  })
 
-	-- use({
-	--   "vim-scripts/vim-auto-save",
-	--   opt = true,
-	--   event = "VimEnter",
-	--   config = function()
-	--     require("plugconfig/vim_auto_save")
-	--   end,
-	-- })
+  --
+  use({
+    "onsails/lspkind.nvim",
+    config = function()
+      require("plugconfig/lspkind")
+    end,
+  })
+  -- use({
+  -- 	"williamboman/nvim-lsp-installer",
+  -- }),
+  use({ "neovim/nvim-lspconfig" })
+  use({ "williamboman/mason.nvim" })
+  use({ "williamboman/mason-lspconfig.nvim" })
 
-	use({
-		"Pocco81/AutoSave.nvim",
-		config = function()
-			require("plugconfig/auto_save_nvim")
-		end,
-		event = "VimEnter",
-	})
-	use({
-		"rmagatti/auto-session",
-		config = function()
-			require("plugconfig/auto_session")
-		end,
-	})
-	use({
-		"rmagatti/session-lens",
-		requires = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
-		config = function()
-			require("session-lens").setup({--[[your custom config--]]
-			})
-		end,
-	})
 
-	use({
-		"vim-jp/vimdoc-ja",
-		config = function()
-			require("plugconfig/vimdoc_ja")
-		end,
-	})
-	-- terminal
-	use({
-		"akinsho/toggleterm.nvim",
-		tag = "v2.*",
-		event = "VimEnter",
-		config = function()
-			require("plugconfig/toggleterm")
-		end,
-	})
-	-- git
-	use({
-		"kdheepak/lazygit.nvim",
-		event = "VimEnter",
-		config = function()
-			require("plugconfig/lazygit")
-		end,
-	})
+  -- use({
+  -- 	"hrsh7th/cmp-vsnip",
+  -- 	"hrsh7th/vim-vsnip",
+  -- 	after = "nvim-cpm",
+  -- 	config = function()
+  -- 		require("plugconfig/vsnip")
+  -- 	end,
+  -- })
+  use({
+    "hrsh7th/vim-vsnip",
+    config = function()
+      require("plugconfig/vsnip")
+    end,
+    requires = {
+      { "hrsh7th/vim-vsnip-integ" },
+    },
+  })
+  --use({ "hrsh7th/cmp-vsnip", after = "nvim-cpm" })
+  -- format and linter
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require("plugconfig/null_ls")
+    end,
+  })
+  --use({"lambdalisue/fern.vim" , opt = true, cmd = {'Fern'}})
 
-	use({ "alaviss/nim.nvim", event = "VimEnter" })
+  use({
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    event = "VimEnter",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("plugconfig/neo-tree")
+    end,
+  })
+  -- use({
+  -- 	"lambdalisue/fern.vim",
+  -- 	opt = true,
+  -- 	event = "VimEnter",
+  --
+  -- 	config = function()
+  -- 		require("plugconfig/fern")
+  -- 	end,
+  -- })
+  -- use({ "lambdalisue/fern-git-status.vim", after = "fern.vim" })
+  -- -- use({ "lambdalisue/nerdfont.vim", after = "fern.vim" })
+  -- use({ "lambdalisue/fern-renderer-nerdfont.vim", after = "fern.vim", requires = { "lambdalisue/nerdfont.vim" } })
+  -- use({ "lambdalisue/glyph-palette.vim", after = "fern.vim" })
+  -- use({ "lambdalisue/fern-bookmark.vim", after = "fern.vim" })
+  use({
+    "t9md/vim-quickhl",
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/vim_quichl")
+    end,
+  })
+  use({
+    "terryma/vim-expand-region",
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/vim_expand_region")
+    end,
+  })
+  use({ "segeljakt/vim-silicon", event = "VimEnter" })
+  -- colorschem
+  use({ "dracula/vim", as = "dracula" })
+  -- use({ "sainnhe/sonokai" })
 
-	-- use({ "vim-denops/denops.vim" })
-	--use({ "lambdalisue/gina.vim"  })
-	-- use({
-	--   "alvan/vim-closetag",
-	--   config = function()
-	--     require("plugconfig/vim_closetag")
-	--   end,
-	-- })
+  --use("rebelot/kanagawa.nvim")
 
-	-- use({
-	--   "rchaser53/insertclosetag",
-	--   config = function()
-	--     require("plugconfig/insertclosetag")
-	--   end,
-	-- })
-	use({
-		"heavenshell/vim-jsdoc",
-		cmd = { "JsDoc" },
-		ft = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
-	})
+  -- use({
+  --   "vim-scripts/vim-auto-save",
+  --   opt = true,
+  --   event = "VimEnter",
+  --   config = function()
+  --     require("plugconfig/vim_auto_save")
+  --   end,
+  -- })
 
-	use({
-		"steelsojka/pears.nvim",
-		config = function()
-			require("plugconfig/pears")
-		end,
-	})
+  use({
+    "Pocco81/AutoSave.nvim",
+    config = function()
+      require("plugconfig/auto_save_nvim")
+    end,
+    event = "VimEnter",
+  })
+  -- use({
+  -- 	"rmagatti/auto-session",
+  -- 	config = function()
+  -- 		require("plugconfig/auto_session")
+  -- 	end,
+  -- })
+  -- use({
+  -- 	"rmagatti/session-lens",
+  -- 	requires = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
+  -- 	config = function()
+  -- 		require("session-lens").setup({--[[your custom config--]]
+  -- 		})
+  -- 	end,
+  -- })
 
-	use({
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("plugconfig/indent_blankline")
-		end,
-	})
-	-- treesitter settins
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		event = "VimEnter",
-		run = ":TSUpdate",
-		config = function()
-			require("plugconfig/treesitter")
-		end,
-	})
-	use({ "p00f/nvim-ts-rainbow", after = "nvim-treesitter" })
+  use({
+    "vim-jp/vimdoc-ja",
+    config = function()
+      require("plugconfig/vimdoc_ja")
+    end,
+  })
+  -- terminal
+  use({
+    "akinsho/toggleterm.nvim",
+    tag = "v2.*",
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/toggleterm")
+    end,
+  })
+  -- git
+  use({
+    "kdheepak/lazygit.nvim",
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/lazygit")
+    end,
+  })
 
-	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
-  use({"JoosepAlviste/nvim-ts-context-commentstring", after = {"nvim-treesitter", "mini.nvim"}})
+  use({ "alaviss/nim.nvim", event = "VimEnter" })
 
-	use({
-		"lewis6991/gitsigns.nvim",
-		event = "VimEnter",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	})
-	use({ "MunifTanjim/nui.nvim" })
+  -- use({ "vim-denops/denops.vim" })
+  --use({ "lambdalisue/gina.vim"  })
+  -- use({
+  --   "alvan/vim-closetag",
+  --   config = function()
+  --     require("plugconfig/vim_closetag")
+  --   end,
+  -- })
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		event = "VimEnter",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-		config = function()
-			require("plugconfig/lualine")
-		end,
-	})
-	use({
-		"echasnovski/mini.nvim",
-		branch = "stable",
-		config = function()
-			require("plugconfig/mini")
-		end,
-	})
-	use("tversteeg/registers.nvim")
-	use({
-		"folke/which-key.nvim",
-		event = "VimEnter",
-		config = function()
-			require("plugconfig/whichi_key")
-		end,
-	})
-	use("ryanoasis/vim-devicons")
-	use({
-		"relastle/vim-colorrange",
-		event = "VimEnter",
-	})
-	-- use({ "lilydjwg/colorizer", opt = true, event = "VimEnter" })
-	use({
-		"norcalli/nvim-colorizer.lua",
-		event = "VimEnter",
-		config = function()
-			require("plugconfig/nvim-colorizer")
-		end,
-	})
-	use({
-		"folke/todo-comments.nvim",
-		requires = "nvim-lua/plenary.nvim",
-		event = "VimEnter",
-		config = function()
-			require("plugconfig/todo-comments")
-		end,
-	})
+  -- use({
+  --   "rchaser53/insertclosetag",
+  --   config = function()
+  --     require("plugconfig/insertclosetag")
+  --   end,
+  -- })
+  use({
+    "heavenshell/vim-jsdoc",
+    cmd = { "JsDoc" },
+    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
+  })
 
-	use({ "machakann/vim-highlightedyank" })
+  use({
+    "steelsojka/pears.nvim",
+    config = function()
+      require("plugconfig/pears")
+    end,
+  })
 
-	use({ "t9md/vim-choosewin", event = "VimEnter" })
+  use({
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("plugconfig/indent_blankline")
+    end,
+  })
+  -- treesitter settins
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    event = "VimEnter",
+    run = ":TSUpdate",
+    config = function()
+      require("plugconfig/treesitter")
+    end,
+  })
+  use({ "p00f/nvim-ts-rainbow", after = "nvim-treesitter" })
 
-	-- use({ "davidgranstrom/nvim-markdown-preview", opt = true, event = "VimEnter" })
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && npm install",
-		event = "VimEnter",
-		setup = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	})
+  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
+  use({ "JoosepAlviste/nvim-ts-context-commentstring", after = { "nvim-treesitter", "mini.nvim" } })
 
-	-- file tree
+  use({
+    "lewis6991/gitsigns.nvim",
+    event = "VimEnter",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  })
+  use({ "MunifTanjim/nui.nvim" })
+
+  use({
+    "nvim-lualine/lualine.nvim",
+    event = "VimEnter",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    config = function()
+      require("plugconfig/lualine")
+    end,
+  })
+  use({
+    "echasnovski/mini.nvim",
+    branch = "stable",
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/mini")
+    end,
+  })
+  use("tversteeg/registers.nvim")
+  use({
+    "folke/which-key.nvim",
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/whichi_key")
+    end,
+  })
+  use("ryanoasis/vim-devicons")
+  use({
+    "relastle/vim-colorrange",
+    event = "VimEnter",
+  })
+  -- use({ "lilydjwg/colorizer", opt = true, event = "VimEnter" })
+  use({
+    "norcalli/nvim-colorizer.lua",
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/nvim-colorizer")
+    end,
+  })
+  use({
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/todo-comments")
+    end,
+  })
+
+  use({ "machakann/vim-highlightedyank" })
+
+  use({ "t9md/vim-choosewin", event = "VimEnter" })
+
+  -- use({ "davidgranstrom/nvim-markdown-preview", opt = true, event = "VimEnter" })
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = "cd app && npm install",
+    event = "VimEnter",
+    setup = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  })
+  use({ 'ekickx/clipboard-image.nvim',
+    event = "VimEnter",
+    config = function()
+      require("plugconfig/clipboard-image")
+    end, })
+
+  -- file tree
 end)
 
 -- vim.cmd([[autocmd BufWritePost lua/config/plugins.lua PackerCompile]])
--- lua/plugconfig/null_ls.lua
+++ lua/plugconfig/null_ls.lua
@@ -1,67 +1,71 @@
 local null_ls = require("null-ls")
 
 local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			--          apply whatever logic you want (in this example, we'll only use null-ls)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
+  vim.lsp.buf.format({
+    timeout_ms = 2000,
+
+    filter = function(client)
+      --          apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
 end
 
 -- if you want to set up formatting on save, you can use this as a callback
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-local opts = { noremap = true, silent = true }
+
 -- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				lsp_formatting(bufnr)
-			end,
-		})
-	end
-end
-
+-- local on_attach =
 null_ls.setup({
-	sources = {
-		-- LuaFormatter off
-		-- null_ls.builtins.completion.spell,
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.black,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
+      })
+    end
+  end
+  ,
 
-		null_ls.builtins.formatting.deno_fmt.with({
-			condition = function(utils)
-				return not (utils.has_file({ ".prettierrc", ".prettierrc.js", "package.json" }))
-			end,
-		}),
-		null_ls.builtins.formatting.prettier.with({
-			condition = function(utils)
-				return utils.has_file({ ".prettierrc", ".prettierrc.js", "package.json" })
-			end,
-			prefer_local = "node_modules/.bin",
-		}),
+  sources = {
+    -- LuaFormatter off
+    -- null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.black,
 
-		-- rust-analyzer
-		null_ls.builtins.formatting.rustfmt,
-		null_ls.builtins.diagnostics.golangci_lint,
-    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.deno_fmt.with({
+      condition = function(utils)
+        return not (utils.has_file({ ".prettierrc", ".prettierrc.js", "package.json" }))
+      end,
+    }),
+        null_ls.builtins.formatting.prettier.with({
+      condition = function(utils)
+        return utils.has_file({ ".prettierrc", ".prettierrc.js", "package.json" })
+      end,
+      prefer_local = "node_modules/.bin",
+    }),
 
-		-- null_ls.builtins.formatting.prettier,
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.formatting.shfmt,
-		null_ls.builtins.diagnostics.shellcheck,
-		null_ls.builtins.formatting.clang_format,
-		-- null_ls.builtins.diagnostics.codespell,
-		null_ls.builtins.formatting.markdownlint,
-		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.formatting.gofmt,
-		-- LuaFormatter on
-	},
-	on_attach = on_attach,
+    -- rust-analyzer
+    null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.diagnostics.golangci_lint,
+    -- null_ls.builtins.formatting.stylua,
+
+    -- null_ls.builtins.formatting.prettier,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.shfmt,
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.formatting.clang_format,
+    -- null_ls.builtins.diagnostics.codespell,
+    null_ls.builtins.formatting.markdownlint,
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.gofmt,
+    -- LuaFormatter on
+    -- null_ls.builtins.formatting.lua_format,
+  },
+  -- on_attach = on_attach,
 })
