db942601cea61eba3ec78f15e8cbc726474f5339
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-08-15 19:22:18 +0900

    どのコマンドモードでもワードをジャンプできるようになった

--- lua/plugconfig/hop.lua
+++ lua/plugconfig/hop.lua
@@ -1,5 +1,5 @@
 require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
 
-vim.api.nvim_set_keymap("n", "Fw", "<cmd>HopWord<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "Fa", "<cmd>HopAnywhere<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "Fl", "<cmd>HopLine<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "Fw", "<cmd>HopWord<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "Fa", "<cmd>HopAnywhere<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "Fl", "<cmd>HopLine<cr>", { noremap = true, silent = true })
