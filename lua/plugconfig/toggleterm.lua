e553f4c9223bd68bcdc019ceb78a98a1e05730e5
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-09-02 14:12:22 +0900

    下に出るオプションを追加した

--- lua/plugconfig/toggleterm.lua
+++ lua/plugconfig/toggleterm.lua
@@ -7,3 +7,12 @@ require("toggleterm").setup({
 		border = "single",
 	},
 })
+-- vim.api.nvim_set_keymap("n", "<A-t>", "ToggleTerm size=40 direction=horizontal", { noremap = true, silent = true })
+vim.cmd([[
+
+autocmd TermEnter term://*toggleterm#*
+      \ tnoremap <silent><c-y> <Cmd>exe v:count1 . "ToggleTerm size=14 direction=horizontal"<CR>
+
+nnoremap <silent><c-y> <Cmd>exe v:count1 . "ToggleTerm size=14 direction=horizontal"<CR>
+inoremap <silent><c-y> <Esc><Cmd>exe v:count1 . "ToggleTerm size=14 direction=horizontal"<CR>
+]])
