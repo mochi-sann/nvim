19bccff4266da369748402f09ad91cd882bfa94f
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-10-09 11:49:14 +0900

    チョット修正

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ vim.cmd([[
 autocmd BufRead,BufEnter *.astro set filetype=astro
 
 
-nmap <Esc><Esc> :nohlsearch<CR><Esc>
+nmap <silent> <Esc><Esc> :nohlsearch<CR><Esc>
 "検索結果のハイライトを、ESC キー連打で解除する。"
 
 tnoremap <Esc> <C-\><C-n>
--- lua/plugconfig/vim_quichl.lua
+++ lua/plugconfig/vim_quichl.lua
@@ -3,5 +3,4 @@ nmap <Space>m <Plug>(quickhl-manual-this)
 xmap <Space>m <Plug>(quickhl-manual-this)
 nmap <Space>M <Plug>(quickhl-manual-reset)
 xmap <Space>M <Plug>(quickhl-manual-reset)
-
 ]])
