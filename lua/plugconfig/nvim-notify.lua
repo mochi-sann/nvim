7a9623e6435ae7c6be5bd7eb928f254ca26e06fa
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-12-01 11:44:22 +0900

    ファイルを読み込んだときに通知を出すのをやめた

--- lua/plugconfig/nvim-notify.lua
+++ lua/plugconfig/nvim-notify.lua
@@ -21,8 +21,8 @@ notify.setup({
 })
 
 vim.cmd([[
-autocmd BufRead * lua vim.notify('BufRead ' .. vim.fn.expand('%'))
-autocmd BufEnter * lua vim.notify('BufEnter ' .. vim.fn.expand('%'))
-autocmd WinEnter * lua vim.notify('WinEnter ' .. vim.fn.winnr())
+" autocmd BufRead * lua vim.notify('BufRead ' .. vim.fn.expand('%'))
+" autocmd BufEnter * lua vim.notify('BufEnter ' .. vim.fn.expand('%'))
+" autocmd WinEnter * lua vim.notify('WinEnter ' .. vim.fn.winnr())
 ]])
 vim.notify = notify
