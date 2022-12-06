da59a285157c60792d2607933445a25f9eb0881e
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-12-06 13:28:15 +0900

    hop.nvim をアップデート

--- lua/plugconfig/hop.lua
+++ lua/plugconfig/hop.lua
@@ -1,6 +1,6 @@
 require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
 
-vim.api.nvim_set_keymap("", "Fw", "<cmd>HopWord<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "Fa", "<cmd>HopAnywhere<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "Fl", "<cmd>HopLine<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("", "Fc", "<cmd>HopWordCurrentLine<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<Space>fw", "<cmd>HopWord<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<Space>fa", "<cmd>HopAnywhere<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<Space>fl", "<cmd>HopLine<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<Space>fc", "<cmd>HopWordCurrentLine<cr>", { noremap = true, silent = true })
