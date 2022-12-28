0d2fb4627de6da2d8bd69a5220d5fddbe0aa88ca
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-12-28 11:16:30 +0900

    update

--- lua/plugconfig/hop.lua
+++ lua/plugconfig/hop.lua
@@ -1,6 +1,8 @@
-require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+require("hop").setup({ keys = "etovxqpdygfblzhkisuran" })
+local vim = vim
 
 vim.api.nvim_set_keymap("", "<Space>fw", "<cmd>HopWord<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<Space>fp", "<cmd>HopPattern<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<Space>fa", "<cmd>HopAnywhere<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<Space>fl", "<cmd>HopLine<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<Space>fc", "<cmd>HopWordCurrentLine<cr>", { noremap = true, silent = true })
