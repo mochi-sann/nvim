a594971fe555c50444b8067f88c984df047159bd
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-01-15 18:43:22 +0900

    update settings

--- init.lua
+++ init.lua
@@ -5,5 +5,5 @@ vim.cmd([[
 autocmd BufRead,BufEnter *.astro set filetype=astro
 nmap <silent> <Esc><Esc> :nohlsearch<CR><Esc>
 tnoremap <Esc> <C-\><C-n>
-colorschem tokyonight-night
+colorschem tokyonight-moon
 ]])
