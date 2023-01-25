1bfcfffb333669356447c534d14025034875d0c8
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-01-25 16:58:27 +0900

    update

--- lua/plugconfig/lualine.lua
+++ lua/plugconfig/lualine.lua
@@ -72,27 +72,6 @@ require("lualine").setup({
 		-- theme = bubbles_theme,
 		component_separators = "|",
 		section_separators = { left = "", right = "" },
+    globalstatus = true
 	},
-	sections = {
-		lualine_a = {
-			{ "mode", separator = { left = "" }, right_padding = 2 },
-		},
-		lualine_b = { "filename", "branch" },
-		lualine_c = { "fileformat" },
-		lualine_x = {},
-		lualine_y = { "filetype", "progress" },
-		lualine_z = {
-			{ "location", separator = { right = "" }, left_padding = 2 },
-		},
-	},
-	inactive_sections = {
-		lualine_a = { "filename" },
-		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = { "location" },
-	},
-	tabline = {},
-	extensions = {},
 })
